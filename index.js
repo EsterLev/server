fbcb893a51e17a48e13e10c1adff166ae862c30a
EstiLev <st3281357@gmail.com>
2022-07-10 18:20:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 const userController = require('./controllers/user.controller');
 const dailyController = require('./controllers/daily.controller');
 const loginController = require('./controllers/login.controller');
-// const authMiddleware = require('./middleware/middleware');
+const authMiddleware = require('./middleware/middleware');
 const meetingController = require('./controllers/meeting.controller');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+// const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
 const port = 3000
-
+// , {customCss}
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -17,7 +20,6 @@ app.use('/users', userController);
 app.use('/daily', dailyController);
 app.use('/login', loginController);
 app.use('/meeting', meetingController);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
