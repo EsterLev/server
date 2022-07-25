fe21e9c9b89210b2264d3365b6d47d1b25a4c584
shiraShr <shirasharabani@gmail.com>
2022-07-25 11:09:22 +0300

    push the changes

--- app.js
+++ app.js
@@ -9,22 +9,26 @@ const meetingController = require('./controllers/meeting.controller');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const db = require('./models/db');
-// const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
-// const port = process.env.PORT || 3030;
-const port =3000
+const dotenv = require('dotenv');
+const { auth, requiresAuth } = require('express-openid-connect');
+const port = process.env.PORT || 3000;
+dotenv.config();
 db.connect();
-// , {customCss}
 app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
-// app.use(express.urlencoded());
-app.use('/users', userController);
-app.use('/daily', dailyController);
-app.use('/login', loginController);
-app.use('/meeting', meetingController);
-
+app.use(auth(authMiddleware));
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+app.use('/users', requiresAuth(), userController);
+app.use('/daily', requiresAuth(), dailyController);
+app.use('/Mylogin', requiresAuth(), loginController);
+app.use('/meeting', requiresAuth(), meetingController);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  
 })
-
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,39 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// const authMiddleware = async (req, res, next) => {
-//     const token =
-//     req.body.token || req.query.token || req.headers["x-access-token"] || req.headers['authorization'].split(' ').pop();
-
-//   if (!token) {
-//     return res.status(403).send("A token is required for authentication");
-//   }
-//   try {
-//     const decoded = jwt.verify(token, config.TOKEN_KEY);
-//     // TODO: check it payload is valid - belongs to existing user & didnt expired
-//     req.user = decoded;
-//   } catch (err) {
-//     return res.status(401).send("Invalid Token");
-//   }
-//   return next();
-// }
-// module.exports = authMiddleware;
-
-
-// const express = require('express');
-// const app = express();
-// const { auth } = require('express-oauth2-jwt-bearer');
-
-// // Authorization middleware. When used, the Access Token must
-// // exist and be verified against the Auth0 JSON Web Key Set.
-// const checkJwt = auth({
-//   audience: 'YOUR_API_IDENTIFIER',
-//   issuerBaseURL: `https://localhost:3000/`,
-// });
-
-
-// module.exports = checkJwt;
-
-
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -42,6 +6,5 @@ const config = {
   clientID: 'g43FTcTXYTXoivfU2DTyzmcaUaEw6t9H',
   issuerBaseURL: 'https://dev-izlo-xu0.us.auth0.com'
 };
-
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 module.exports = config;
